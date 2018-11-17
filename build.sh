fe82f74fc80e62c508655b675217f2af53385e34
DoubleU23 <dev@project23.org>
2018-11-17 01:31:33 +0100

    fix(build): :green_heart: mkdir dist

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 #
-# TBD: refactor: build scripts with ? nodemon | rollup | backpack?
+rm dist/ -rf
+mkdir dist
 npx babel ./src/index.js --out-file ./dist/index.js --source-maps inline --presets minify
 npx babel ./src/extendGitmoji.js --out-file ./dist/extendGitmoji.js --source-maps inline --presets minify
