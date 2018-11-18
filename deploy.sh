6b99375b62d370ee0395649a93c3aede21f27ebe
DoubleU23 <dev@project23.org>
2018-11-18 02:58:48 +0100

    fix(deps): :heavy_plus_sign: package-lock

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-npm i semantic-release @semantic-release/release-notes-generator @semantic-release/changelog @semantic-release/git @semantic-release/github @semantic-release/npm
-# run
-npx semantic-release
